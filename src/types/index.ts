742918c51c5fec3c89c219a502653edc63734ffe
Bartek <bartek.kaszowski@spoko.pl>
2023-09-08 22:11:52 +0200

    errors and film type

---
+++ src/types/index.ts
@@ -0,0 +1,1 @@
+export {RawFilm, SimplifiedFilm} from './film'
---
+++ src/utils/errors.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+export class InternalServiceError extends Error {
+    constructor(message: string, public statusCode?: number) {
+        super(message);
+        this.statusCode = statusCode || 500;
+    }
+}
+export class ValidationError extends Error {
+    constructor(message:string, public statusCode?:number) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+
+export const handleError = (err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+
+    const statusCode = res.statusCode ? res.statusCode : 500;
+    res.status(statusCode);
+    res
+        .json({
+            message: err instanceof Error ? err.message : 'Sorry, please try again later.',
+        });
+};
