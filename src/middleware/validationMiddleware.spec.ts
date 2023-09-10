77ca0e2b996820b4efdab48e4b664770b0959a49
Bartek <bartek.kaszowski@spoko.pl>
2023-09-10 16:16:48 +0200

    validation additional fields whitelisting and tests for it

---
+++ src/middleware/validationMiddleware.spec.ts
@@ -0,0 +1,75 @@
+
+
+import request from 'supertest';
+import express, {NextFunction, Response, Request} from 'express';
+import { validationMiddleware } from './validationMiddleware';
+import {CreateFavoriteListDTO} from "../Favorites/dto/CreateFavoriteList.dto";
+
+
+
+const app = express();
+app.use(express.json());
+
+// Define an example POST endpoint using the validation middleware
+app.post(
+    '/favorites',
+    validationMiddleware(CreateFavoriteListDTO),
+    (req, res) => res.status(201).send('Created')
+);
+
+// Mock objects for request and response
+const mockRequest = {} as Request;
+const mockResponse = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+} as unknown as Response;
+
+// Mock object for the next function in the middleware
+const mockNext: NextFunction = jest.fn();
+describe('Validation Middleware', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+    it('should pass validation and return 201', async () => {
+        await request(app)
+            .post('/favorites')
+            .send({ name: 'Fav List', filmIds: [1, 2, 3] })
+            .expect(201);
+    });
+
+    it('should fail validation and return 400', async () => {
+        await request(app)
+            .post('/favorites')
+            .send({ name: '', filmIds: 'not an array' })
+            .expect(400);
+    });
+
+    it('should fail when no body is sent', async () => {
+        await request(app)
+            .post('/favorites')
+            .send({})
+            .expect(400);
+    });
+
+    it('should disallow additional fields', async () => {
+        await request(app)
+            .post('/favorites')
+            .send({ name: 'Fav List', filmIds: [1, 2, 3], extraField: 'additional field text'  })
+            .expect(400);
+    });
+    // Test case to check if the next function is called for valid requests
+    it('should call next() once if validation passes', async () => {
+        const middleware = validationMiddleware(CreateFavoriteListDTO);
+
+        // Set up a valid request body
+        mockRequest.body = { name: 'Fav List', filmIds: [1, 2, 3] };
+
+        // Invoke the middleware
+        await middleware(mockRequest, mockResponse, mockNext);
+
+        // Verify that next() was called exactly once
+        expect(mockNext).toHaveBeenCalledTimes(1);
+    });
+
+
+});
--- src/middleware/validationMiddleware.ts
+++ src/middleware/validationMiddleware.ts
@@ -7,7 +7,10 @@ import { Request, Response, NextFunction } from 'express';
 export function validationMiddleware<T extends object>(type: new () => T) {
     return async (req: Request, res: Response, next: NextFunction) => {
         const input = plainToInstance(type, req.body);
-        const errors = await validate(input);
+        const errors = await validate(input, {
+            whitelist: true, // ensures that only properties decorated with class-validator decorators are allowed
+            forbidNonWhitelisted: true, // rejects request if it contains non-whitelisted properties
+        });
 
         if (errors.length > 0) {
             const message = errors.map((error: ValidationError) => Object.values(error.constraints)).join(', ');
