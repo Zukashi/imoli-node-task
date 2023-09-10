54aaf6821ee5bbec67cc7610f2792668d9437152
Bartek <bartek.kaszowski@spoko.pl>
2023-09-10 16:21:31 +0200

    grouped for valid / invalid scenarios

--- src/middleware/validationMiddleware.spec.ts
+++ src/middleware/validationMiddleware.spec.ts
@@ -30,46 +30,51 @@ describe('Validation Middleware', () => {
     beforeEach(() => {
         jest.clearAllMocks();
     });
-    it('should pass validation and return 201', async () => {
-        await request(app)
-            .post('/favorites')
-            .send({ name: 'Fav List', filmIds: [1, 2, 3] })
-            .expect(201);
+
+    // Group for valid scenarios
+    describe('Valid Request Body', () => {
+        it('should pass validation and return 201', async () => {
+            await request(app)
+                .post('/favorites')
+                .send({ name: 'Fav List', filmIds: [1, 2, 3] })
+                .expect(201);
+        });
+        // Test case to check if the next function is called for valid requests
+        it('should call next() once if validation passes', async () => {
+            const middleware = validationMiddleware(CreateFavoriteListDTO);
+
+            // Set up a valid request body
+            mockRequest.body = { name: 'Fav List', filmIds: [1, 2, 3] };
+
+            // Invoke the middleware
+            await middleware(mockRequest, mockResponse, mockNext);
+
+            // Verify that next() was called exactly once
+            expect(mockNext).toHaveBeenCalledTimes(1);
+        });
     });
 
-    it('should fail validation and return 400', async () => {
-        await request(app)
-            .post('/favorites')
-            .send({ name: '', filmIds: 'not an array' })
-            .expect(400);
+    // Group for invalid scenarios
+    describe('Invalid Request Body', () => {
+        it('should fail validation and return 400', async () => {
+            await request(app)
+                .post('/favorites')
+                .send({ name: '', filmIds: 'not an array' })
+                .expect(400);
+        });
+
+        it('should fail when no body is sent', async () => {
+            await request(app)
+                .post('/favorites')
+                .send({})
+                .expect(400);
+        });
+
+        it('should disallow additional fields', async () => {
+            await request(app)
+                .post('/favorites')
+                .send({ name: 'Fav List', filmIds: [1, 2, 3], extraField: 'additional field text'  })
+                .expect(400);
+        });
     });
-
-    it('should fail when no body is sent', async () => {
-        await request(app)
-            .post('/favorites')
-            .send({})
-            .expect(400);
-    });
-
-    it('should disallow additional fields', async () => {
-        await request(app)
-            .post('/favorites')
-            .send({ name: 'Fav List', filmIds: [1, 2, 3], extraField: 'additional field text'  })
-            .expect(400);
-    });
-    // Test case to check if the next function is called for valid requests
-    it('should call next() once if validation passes', async () => {
-        const middleware = validationMiddleware(CreateFavoriteListDTO);
-
-        // Set up a valid request body
-        mockRequest.body = { name: 'Fav List', filmIds: [1, 2, 3] };
-
-        // Invoke the middleware
-        await middleware(mockRequest, mockResponse, mockNext);
-
-        // Verify that next() was called exactly once
-        expect(mockNext).toHaveBeenCalledTimes(1);
-    });
-
-
 });
