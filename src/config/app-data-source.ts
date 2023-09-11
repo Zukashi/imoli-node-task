54af7e8d5882de61fc4d88960e3bf13e238eab59
Bartek <bartek.kaszowski@spoko.pl>
2023-09-11 11:42:09 +0200

    different config for localhost and deploy

--- src/config/app-data-source.ts
+++ src/config/app-data-source.ts
@@ -3,12 +3,25 @@ import {Character} from "../Character/entities/Character.entity";
 import {Favorite} from "../Favorites/entities/Favorite.entity";
 import {Film} from "../Film/entities/Film.entity";
 
-export const myDataSource = new DataSource({
-    type: "postgres",
-    url:process.env.DATABASE_HOST,
-    ssl: {
-        rejectUnauthorized: false,
-    },
-    synchronize:true,
-    entities: [Character,Favorite,Film],
-})
+export let myDataSource:DataSource
+    if(process.env.DATABASE_URL){
+        myDataSource = new DataSource({
+            type: "postgres",
+            url:process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false,
+            },
+            synchronize:true,
+            entities: [Character,Favorite,Film],
+        })
+    }else{
+        myDataSource = new DataSource({
+            type: "postgres",
+            host: "localhost",
+            port: 5432,
+            username: "postgres",
+            password: "",
+            database: "postgres",
+            entities: [Character,Favorite,Film],
+        })
+    }
