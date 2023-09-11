77f6df63886d06582330fdc57584ffb2deefac9a
Bartek <bartek.kaszowski@spoko.pl>
2023-09-11 10:56:45 +0200

    rejectUnauthorized

--- src/config/app-data-source.ts
+++ src/config/app-data-source.ts
@@ -7,7 +7,7 @@ export const myDataSource = new DataSource({
     type: "postgres",
     url:process.env.DATABASE_HOST,
     ssl: {
-        rejectUnauthorized: false,  // This is for development, use `true` in production
+        rejectUnauthorized: true,  // This is for development, use `true` in production
     },
     synchronize:true,
     entities: [Character,Favorite,Film],
